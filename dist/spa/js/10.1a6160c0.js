8d82c7d1962e73d1936c6d92de60ac946d2e8b34
wilson-pc <wilverhidalgo@outlook.com>
2020-09-28 12:32:56 -0400

    dist

---
+++ dist/spa/js/10.1a6160c0.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[10],{"4a93":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("q-page",{attrs:{padding:""}})},s=[],c={},l=c,o=t("2877"),p=t("9989"),r=t("eebe"),u=t.n(r),i=Object(o["a"])(l,a,s,!1,null,null,null);n["default"]=i.exports;u()(i,"components",{QPage:p["a"]})}}]);
